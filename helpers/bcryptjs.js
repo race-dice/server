4dbacb8559ca4947e58a3f2944a78b3cdaddc372
evans <karlinevans333@gmail.com>
2019-10-17 17:57:40 +0700

    userserver

---
+++ helpers/bcryptjs.js
@@ -0,0 +1,12 @@
+const bcryptjs = require('bcryptjs')
+const salt = bcryptjs.genSaltSync(11)
+
+function hashPassword (password) {
+    return bcryptjs.hashSync(password,salt)
+}
+
+function comparePassword(password,hashPassword){
+    return bcryptjs.compareSync(password,hashPassword)
+}
+
+module.exports = {hashPassword,comparePassword}
