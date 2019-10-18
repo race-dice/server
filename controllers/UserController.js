b9528bdd7ee54420d7459ea9cd91cfeef64dca1d
Ricky <audrickong.ao@gmail.com>
2019-10-18 14:23:38 +0700

    stash

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -14,9 +14,16 @@ class UserController {
             .then(user => {
                 let payload = {username:user.username, _id:user._id}
                 let token = generateToken(payload)
-                res.status(201).json({token,username})
+                res.status(201).json({token,username, id: user._id})
             })
             .catch(next)
+    }
+    static updateRoomId(req ,res, next) {
+        const { _id } = req.loggedUser
+        const { roomid } = req.params
+        User.updateOne({ _id }, { room: roomid }).exec()
+        .then(() => { res.status(200).json({ msg: "success" }) })
+        .catch(console.log)
     }
     static showall (req, res, next){
         const { roomid } = req.params
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 const UserController = require('../controllers/UserController')
+const { authentication, authorization } = require("../middlewares/auth")
 
 router.post('/register', UserController.register)
 router.get('/:roomid', UserController.showall)
+router.use(authentication)
+router.patch("/:roomid", UserController.updateRoomId)
 
 module.exports = router
